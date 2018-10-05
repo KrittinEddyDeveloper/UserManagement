c238a15ee1bf01dbd140c697354de3daf11cd835
eddy-krittin <krittin@iconext.co.th>
2018-10-05 11:18:15 +0700

    Initial project

---
+++ PracticeDotNetCore/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PracticeDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
